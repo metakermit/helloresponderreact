9609b777a7be42a22a06bc19ae9a86b96615e8c4
Dražen Lučanin <kermit666@gmail.com>
2018-10-23 00:10:18 +0200

    initial commit

---
+++ api.py
@@ -0,0 +1,12 @@
+import responder
+
+api = responder.API()
+
+api.add_route("/", static=True)
+
+@api.route("/admin")
+async def greet_world(req, resp):
+    resp.content = api.template('admin.html')
+
+if __name__ == '__main__':
+    api.run()
