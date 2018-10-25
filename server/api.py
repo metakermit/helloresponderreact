46b8e28f3f5ab420572dce446a6bfea42dc60df9
Dražen Lučanin <kermit666@gmail.com>
2018-10-25 16:56:00 +0200

    Docker support & bin scripts

--- server/api.py
+++ server/api.py
@@ -8,5 +8,8 @@ api.add_route("/", static=True)
 async def greet_world(req, resp):
     resp.content = api.template('admin.html')
 
+def start():
+    api.run(address="0.0.0.0")
+
 if __name__ == '__main__':
-    api.run()
+    start()
